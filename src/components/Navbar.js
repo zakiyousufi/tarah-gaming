acea22c697a93865950df73e3ab0a9e40f374f23
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2023-10-14 14:00:55 +0500

    Create navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assests/Logo.png";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleLogoClick = () => {
+    if (!showLinks) {
+      setShowLinks(true);
+    }
+    else {
+      setShowLinks(false);
+    }
+  };
+
   return (
     <div className="navbar">
-      <img className="logo" src={Logo} alt="logo" />
-      <div className="links">
-        <a className="link" href="#home">Home</a>
-        <a className="link" href="#game">Games</a>
-        <a className="link" href="#moreApps">More Apps</a>
-        <a className="link" href="#contact">Contact</a>
+      <img
+        className="logo"
+        src={Logo}
+        alt="logo"
+        onClick={handleLogoClick}
+      />
+      <div className={`links ${showLinks ? "open" : "close"}`}>
+        <a className="link" href="#home">
+          Home
+        </a>
+        <a className="link" href="#game">
+          Games
+        </a>
+        <a
+          className="link"
+          target="_blank"
+          rel="noreferrer"
+          href="https://zakiyousufi-portfolio.vercel.app/#projects"
+        >
+          More Apps
+        </a>
+        <a className="link" href="#contact">
+          Contact
+        </a>
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
